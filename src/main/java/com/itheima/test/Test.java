ee1f23459c57f87ab6a7dd51e088fcec4b01cec5
admin <773594484@qq.com>
2019-08-10 18:47:00 +0800

    diyici

--- src/main/java/com/itheima/test/Test.java
+++ src/main/java/com/itheima/test/Test.java
@@ -2,7 +2,9 @@ package com.itheima.test;
 
 public class Test {
 
+    private static String name ;
+
     public static void main(String[] args) {
-        System.out.println("hello world1");
+        System.out.println("hello world2");
     }
 }
