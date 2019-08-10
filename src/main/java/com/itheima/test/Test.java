a741f943cca0d8f39055e5b7709f441c946c8cb5
admin <773594484@qq.com>
2019-08-10 19:08:22 +0800

    第三次

--- src/main/java/com/itheima/test/Test.java
+++ src/main/java/com/itheima/test/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
     private static String name ;
 
+    private static String b;
+
     public static void main(String[] args) {
         System.out.println("hello world2");
     }
